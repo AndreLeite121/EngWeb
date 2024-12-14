f0aadcae7a8cf09aa1035124a3eee8d97e62820f
AndreLeite121 <andreribeiro139@gmail.com>
2024-12-14 11:40:22 -0300

    correcao

--- tarefas/manage.py
+++ tarefas/manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Configuração do módulo de configurações do Django
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tarefas.settings')
     try:
         from django.core.management import execute_from_command_line
